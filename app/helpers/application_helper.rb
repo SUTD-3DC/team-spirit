e5e956e30ac30f6cbfca2feb22f6bdcaec98163e
yaojie <limyaojie93@gmail.com>
2016-05-26 22:31:57 +0800

    show only one date if it is a single day event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     "All rights reserved #{Time.now.year}".html_safe
   end
 
+  def event_date(event)
+    if event.start_date == event.end_date
+      event.start_date.strftime('%d %B %Y')
+    else
+      "#{event.start_date.strftime('%d %B %Y')} to #{event.end_date.strftime('%d %B %Y')}"
+    end
+  end
 end
