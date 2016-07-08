d786c8c2203dd9947300e5a434b4df385314d749
yaojie <limyaojie93@gmail.com>
2016-07-08 09:42:23 +0800

    + puts restart puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       '/var/www/#{fetch(:application)}'
-set :puma_bind,       'unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock'
+set :puma_bind,       'unix://#{shared_path}/tmp/sockets/puma.sock'
 set :puma_state,      '#{shared_path}/tmp/pids/puma.state'
 set :puma_pid,        '#{shared_path}/tmp/pids/puma.pid'
 set :puma_access_log, '#{release_path}/log/puma.error.log'
@@ -71,6 +71,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      puts 'Restarting puma server..'
       invoke 'puma:restart'
     end
   end
