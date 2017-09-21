b64bb35dd44c52afa404e9bfd968e48a5bc27c09
yaojie <limyaojie93@gmail.com>
2017-09-21 20:12:47 +0800

    update event table to show both events and workshops

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def index
-    @events = Event.where(event_type: 0)
-    @workshops = Event.where(event_type: 1)
+    @events = Event.all
   end
 end
