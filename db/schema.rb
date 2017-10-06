ab848cabaa2011c5f4d12cdfe47b44cbee897243
TAYTS <taytzushieh@gmail.com>
2017-10-06 23:52:38 +0800

    Add new column 'image_uid' to database to handle 'Dragonfly' data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921121633) do
+ActiveRecord::Schema.define(version: 20171006155100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170921121633) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "link"
+    t.string   "image_uid"
     t.index ["event_type"], name: "index_events_on_event_type", using: :btree
   end
 
