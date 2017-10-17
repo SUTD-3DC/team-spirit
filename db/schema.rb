f14fc56e3a09f0c9633061acc7587169df963284
TAYTS <taytzushieh@gmail.com>
2017-10-17 10:14:14 +0800

    Change the column name 'start_time' & 'end_time' to 'start_date' and 'end_date'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016131505) do
+ActiveRecord::Schema.define(version: 20171017021306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20171016131505) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.date     "start_time"
-    t.date     "end_time"
+    t.date     "start_date"
+    t.date     "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
