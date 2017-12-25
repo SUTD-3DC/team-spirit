5fb5ae88868c1627878bf419ffab89ddcccc7b41
TAYTS <taytzushieh@gmail.com>
2017-12-25 23:55:24 +0800

    Add validation for all the fields in the project model and verify the link

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,21 @@
 class Project <  ActiveRecord::Base
+  before_validation :check_link_heading
+  validates :title, :description, :start_date, :end_date, :link, presence: :true
+  validates_format_of :link, with: /\A(http(s?):\/\/www.){1}.+(.com){1}/, message: "The link is invalid.", allow_blank: :true
   has_many :pictures, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :pictures, reject_if: :all_blank, allow_destroy: true
+
+  def check_link_heading
+    if !self.link.empty?
+      self.link.strip!
+      if !self.link.match(/\A(http:\/\/)/)
+        append
+      end
+    end
+  end
+
+  def append
+      self.link = "http://" + self.link
+  end
+
 end
