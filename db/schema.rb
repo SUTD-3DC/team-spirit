3a162de95a39546cdf974d1e7c154195184081ee
TAYTS <taytzushieh@gmail.com>
2017-12-25 23:35:55 +0800

    Remove 'image_url' from event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225153115) do
+ActiveRecord::Schema.define(version: 20171225153518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20171225153115) do
     t.date     "start_date"
     t.date     "end_date"
     t.text     "description"
-    t.string   "image_url"
     t.string   "location"
     t.integer  "event_type"
     t.datetime "created_at",  null: false
