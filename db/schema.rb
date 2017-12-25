92949f0e16e283f96faf22667c9d4f8596e1b0ac
TAYTS <taytzushieh@gmail.com>
2017-12-25 23:32:14 +0800

    Remove 'project_id' column from picture model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223082101) do
+ActiveRecord::Schema.define(version: 20171225153115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,13 +38,11 @@ ActiveRecord::Schema.define(version: 20171223082101) do
   create_table "pictures", force: :cascade do |t|
     t.string   "image_uid"
     t.string   "image_thumb_uid"
-    t.integer  "project_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "imageable_type"
     t.integer  "imageable_id"
     t.index ["imageable_type", "imageable_id"], name: "index_pictures_on_imageable_type_and_imageable_id", using: :btree
-    t.index ["project_id"], name: "index_pictures_on_project_id", using: :btree
   end
 
   create_table "projects", force: :cascade do |t|
@@ -67,5 +65,4 @@ ActiveRecord::Schema.define(version: 20171223082101) do
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
-  add_foreign_key "pictures", "projects"
 end
