5cf359d997f1c391e0d26c7668d8fa7b37e9b0bd
TAYTS <taytzushieh@gmail.com>
2017-12-25 23:58:20 +0800

    Add validation for the presence of the link in the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
   before_validation :check_link_heading
-  validates :title, :description, :start_date, :end_date, :location, :event_type, presence: :true
+  validates :title, :description, :start_date, :end_date, :location, :event_type, :link, presence: :true
   validates_format_of :link, with: /\A(http(s?):\/\/www.){1}.+(.com){1}/, message: "The link is invalid.", allow_blank: :true
   has_one :picture, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :picture, reject_if: :all_blank, allow_destroy: true
-  
+
   TYPES = %w(Event Workshop).freeze
 
   def check_link_heading
